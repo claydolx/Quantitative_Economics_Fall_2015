d3528048f00d5f8a753d9c10cd4b0c1d65071c7c
claydolx <claydolx@foxmail.com>
2015-11-10 21:39:53 +0800

    Create Lec 6.py

---
+++ Lec 6.py
@@ -0,0 +1,29 @@
+#1 
+#过两天我自己都看不懂了
+#[-1]取累积和式的最后一项
+def p(x,coeff): return((((np.zeros(len(coeff))+x).cumprod()*coeff).cumsum())[-1])
+
+#2
+def sample(q):
+    from  random import uniform
+    U = uniform(0,1)
+    f = (np.zeros(len(q))+q).cumsum()
+    return (np.searchsorted(f,U))
+
+class discreteRV:
+    def __init__(self, q):
+        self.q = q
+    def draw(self):
+        from  random import uniform
+        U = uniform(0,1)
+        f = (np.zeros(len(self.q))+self.q).cumsum()
+        return (np.searchsorted(f,U))
+		
+#3
+class ECDF:
+    def __init__(self, obs):
+        self.obs = obs
+    def __call__(self, x):
+        return sum(1 - np.searchsorted([x],self.obs))/len(self.obs)
+    def plots(self, a, b):
+        return self(b)-self(a)
