81b5d69a33ae9f39e803142f84d9b557ed4237f2
claydolx <claydolx@foxmail.com>
2015-11-10 21:46:42 +0800

    Update Lec 6.py

--- Lec 6.py
+++ Lec 6.py
@@ -1,3 +1,6 @@
+import numpy as np
+from random import uniform
+
 #1 
 #过两天我自己都看不懂了
 #[-1]取累积和式的最后一项
@@ -14,7 +17,6 @@ class discreteRV:
     def __init__(self, q):
         self.q = q
     def draw(self):
-        from  random import uniform
         U = uniform(0,1)
         f = (np.zeros(len(self.q))+self.q).cumsum()
         return np.searchsorted(f,U)
